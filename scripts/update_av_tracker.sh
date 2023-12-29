6f18fbec6356994c1b64936003adbf958a4c1338
Dheshan Mohandass <dmohandass@mgh.harvard.edu>
2023-12-29 09:29:19 -0500

    fix: cronjobs
    Signed-off-by: Dheshan Mohandass <dmohandass@mgh.harvard.edu>

--- scripts/update_av_tracker.sh
+++ scripts/update_av_tracker.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-source activate /home/dm2637/miniforge3/envs/jupyter
+source /home/dm2637/miniforge3/etc/profile.d/conda.sh
 
+conda activate /home/dm2637/miniforge3/envs/jupyter
+
+export HOME=/root
 export REPO_ROOT=/home/dm2637/dev/av-pipeline-tracker
 cd $REPO_ROOT
 
-/home/dm2637/dev/av-pipeline-tracker/tracker/scripts/track_logs.py
+git config --global --add safe.directory $REPO_ROOT
+
+echo 'Starting Logs tracker...'
+$REPO_ROOT/tracker/scripts/track_logs.py
 
